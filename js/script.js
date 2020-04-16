34cafe6ab16179ce09c770029541342de7c93782
calvinteng122 <59907179+calvinteng122@users.noreply.github.com>
2020-04-16 00:26:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function init(){	
-	var input = document.getElementById("entryinput");
-        var button = document.getElementById("entrybutton");
-	var output = document.getElementById("textoutput");
-	alert(name.textContent + ": " + input.value);
-	output.innerHTML = input.value;
+	var name = document.getElementsByClassName("center")[1];
+        var input = document.getElementById('entryinput');
+        var output = document.getElementById('textoutput');
+        alert(name.textContent + ": " + input.value);
+        output.innerHTML = input.value;
 }
 
-window.addEventListener("click", init);
+var button = document.getElementById('entrybutton');
+button.addEventListener('click', init);
